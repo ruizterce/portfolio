0cf25cee3741c96088a9f467707f9b2d14c018b7
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-12-13 01:36:11 +0100

    feat(Welcome): add hover card interaction
    - Implemented hover card functionality for specific words
    - Added content for hover cards with icons and descriptions for each
    hovered word
    - Integrated smooth scaling animation for hover cards with `showCard`
    state
    - Fixed positioning of hover cards based on mouse position using
    `onMouseMove`
    - Used `darkTheme:invert` utility for consistent icon visibility in
    dark mode
    - Added icon svgs under `src/assets/icons`

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,139 @@
 import { Tilt } from "react-tilt";
+import { useState, useRef } from "react";
 
 const Welcome = () => {
+  const [hoveredWord, setHoveredWord] = useState(null);
+  const [cardPosition, setCardPosition] = useState({ x: 0, y: 0 });
+  const [showCard, setshowCard] = useState(false);
+  const timeoutId = useRef(null);
+
+  const hoverContent = {
+    ruizterce: <h1>Passionate about coding and problem-solving.</h1>,
+    "Full Stack Dev": (
+      <div className="grid grid-cols-3 gap-4">
+        <img
+          src="/src/assets/icons/git-plain-wordmark.svg"
+          alt="Git"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/html5-plain-wordmark.svg"
+          alt="HTML5"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/css3-plain-wordmark.svg"
+          alt="CSS3"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/javascript-plain.svg"
+          alt="JavaScript"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/typescript-plain.svg"
+          alt="TypeScript"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/vitejs-plain.svg"
+          alt="Vite"
+          className="h-16 w-auto darkTheme:invert"
+        />
+        <img
+          src="/src/assets/icons/ionic-original-wordmark.svg"
+          alt="Ionic"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/react-original-wordmark.svg"
+          alt="React"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/tailwindcss-plain-wordmark.svg"
+          alt="TailwindCSS"
+          className="h-16 w-auto darkTheme:invert"
+        />
+        <img
+          src="/src/assets/icons/axios-plain-wordmark.svg"
+          alt="Axios"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/postman-plain-wordmark.svg"
+          alt="Postman"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/nodejs-plain-wordmark.svg"
+          alt="Node.js"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/postgresql-original-wordmark.svg"
+          alt="PostgreSQL"
+          className="h-16 w-auto darkTheme:invert"
+        />
+        <img
+          src="/src/assets/icons/prisma-original-wordmark.svg"
+          alt="Prisma"
+          className="h-16 w-auto darkTheme:invert"
+        />
+        <img
+          src="/src/assets/icons/express-original-wordmark.svg"
+          alt="Express"
+          className="h-16 w-auto darkTheme:invert"
+        />
+      </div>
+    ),
+    Barcelona: (
+      <div className="grid grid-cols-3 gap-2">
+        <img
+          src="/src/assets/icons/barcelona-sagrada.svg"
+          alt="Sagrada Familia"
+          className="h-16 w-auto darkTheme:invert"
+        />
+        <img
+          src="/src/assets/icons/es-ct.svg"
+          alt="Catalonia"
+          className="h-16 w-auto"
+        />
+        <img
+          src="/src/assets/icons/es.svg"
+          alt="Spain"
+          className="h-16 w-auto"
+        />
+      </div>
+    ),
+  };
+
+  const handleMouseEnter = (word, event) => {
+    clearTimeout(timeoutId.current); // Cancel any pending hide operation
+    setHoveredWord(word);
+    setCardPosition({ x: event.clientX, y: event.clientY });
+
+    setTimeout(() => {
+      setshowCard(true);
+    }, 30); // Slight delay to trigger the animation
+  };
+
+  const handleMouseMove = (event) => {
+    if (hoveredWord) {
+      setCardPosition({ x: event.clientX, y: event.clientY });
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setshowCard(false); // Trigger the disappearance animation
+
+    // Remove the card after the animation completes
+    timeoutId.current = setTimeout(() => {
+      setHoveredWord(null);
+    }, 500);
+  };
+
   const tiltOptions = {
     reverse: true,
     max: 35,
@@ -12,21 +145,67 @@ const Welcome = () => {
     reset: false,
     easing: "cubic-bezier(.03,.98,.52,.99)",
   };
+
   return (
-    <div className="h-full w-full bg-light text-dark flex flex-col items-center justify-center">
+    <div
+      className="h-full w-full bg-light text-dark flex flex-col items-center justify-center"
+      onMouseMove={handleMouseMove}
+    >
       <Tilt
         options={tiltOptions}
         className="h-full w-full flex justify-center items-center"
       >
-        <div className="m-6 text-center w-80 leading-10">
+        <div className="m-6 text-center w-1/2 leading-10">
           <h1>Welcome!</h1>
           <p>
-            <span className="text-6xl">ruizterce </span>is a passionate
-            <span className="text-2xl"> Full Stack Dev</span> based in
-            <span className="text-4xl"> Barcelona, Spain</span>
+            I&apos;m
+            <span
+              className="text-5xl hover:cursor-pointer"
+              onMouseEnter={(e) => handleMouseEnter("ruizterce", e)}
+              onMouseLeave={handleMouseLeave}
+            >
+              ruizterce
+            </span>
+            , a passionate
+            <span
+              className="text-4xl hover:cursor-pointer"
+              onMouseEnter={(e) => handleMouseEnter("Full Stack Dev", e)}
+              onMouseLeave={handleMouseLeave}
+            >
+              {" "}
+              Full Stack Developer
+            </span>{" "}
+            with a love for crafting efficient and elegant digital solutions. I
+            specialize in building responsive web applications, scalable backend
+            systems, and seamless user experiences. Based in
+            <span
+              className="text-3xl hover:cursor-pointer"
+              onMouseEnter={(e) => handleMouseEnter("Barcelona", e)}
+              onMouseLeave={handleMouseLeave}
+            >
+              {" "}
+              Barcelona, Spain
+            </span>{" "}
+            .
           </p>
         </div>
       </Tilt>
+
+      {/* Hover Card */}
+      {hoveredWord && (
+        <div
+          className={`absolute z-10 bg-light text-dark shadow-lg p-4 rounded transition-all duration-500 ease-out darkTheme:border border-dark ${
+            showCard ? "scale-100 opacity-1" : "scale-0 opacity-0"
+          }`}
+          style={{
+            left: `${cardPosition.x + 50}px`,
+            top: `${cardPosition.y - 50}px`,
+            transformOrigin: "left",
+          }}
+        >
+          {hoverContent[hoveredWord]}
+        </div>
+      )}
     </div>
   );
 };
