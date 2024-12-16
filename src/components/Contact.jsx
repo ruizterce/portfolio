204a8b2c7fa544b5dc981903c401b8aa396aae86
ruizterce <127963868+ruizterce@users.noreply.github.com>
2024-12-16 18:13:35 +0100

    feat: add animated title to `Contact` component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,13 @@ import PropTypes from "prop-types";
 const Contact = ({ isCurrentSection }) => {
   return (
     <div className="h-screen w-screen bg-light flex flex-col items-center justify-center">
+      <h1
+        className={`absolute text-3xl font-nunito font-bold text-secondary translate-y-[-160px] transition-all duration-1000 ease-out ${
+          isCurrentSection ? "" : "scale-0 opacity-0"
+        }`}
+      >
+        Contact
+      </h1>
       <div className="relative h-full flex flex-col items-center">
         <span
           className={`text-medium absolute bottom-20 transition-all duration-1000 ${
